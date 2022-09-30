bc349b93ed494ada4f74259f0d9ad597f92518f0
PatrikZh <pzheji@actirana.edu.al>
2022-09-30 10:57:16 -0700

    It dosen't take an genius to do UNIX, but a person who has passion towards it

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the selected text,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
