db481e4364cdd5bc7fbafd53a36de3ffb57ba952
PatrikZh <pzheji@actirana.edu.al>
2022-10-04 05:40:35 -0700

    Comma and space in numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+	for (i = '0'; i <= '9'; i++)
+	{  	
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+
+	}	
+	putchar('\n');
+	return (0);
+}
