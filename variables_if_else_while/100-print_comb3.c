7b09e04d3a6ff3b891ecc6729d76c6f06bf02826
PatrikZh <pzheji@actirana.edu.al>
2022-10-04 07:56:27 -0700

    Two digits listed

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,17 +12,19 @@ int main(void)
 
 	for (i = '0'; i <= '8'; i++)
 	{
-	for (p = '1'; p <= '9'; p++)
-	{
-		if (i < p)
-
-		putchar(i);
-
-		putchar(p);
+		for (p = '1'; p <= '9'; p++)
+		{
+			if (i < p)
+			{
+				putchar(i);
+				putchar(p);
+			}
 		if (i != '8' || p != '9')
-			
-			putchar(',');
+		{	putchar(',');
 			putchar(' ');
-	}
+		}
+		}
+		}
+	putchar('\n');
 	return  (0);
-	}
+}
