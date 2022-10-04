dfa5c6cb76ff4269f5a9150cb3ffd31ceaa7167e
PatrikZh <pzheji@actirana.edu.al>
2022-10-04 07:33:44 -0700

    Adventure

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 int i, p;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+	for (p = '1'; p <= '9'; p++)
+	{
+		if (i < p)
+
+		putchar(i);
+
+		putchar(p);
+		if (i != '8' || p != '9')
+			
+			putchar(',');
+			putchar(' ');
+	}
+	return  (0);
+	}
