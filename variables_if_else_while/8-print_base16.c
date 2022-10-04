534f76843d093f1f7020f7b6253a09d85d2adb43
PatrikZh <pzheji@actirana.edu.al>
2022-10-04 05:18:59 -0700

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char x;
+
+	for (i = 48; i <= 57 ; i++)
+		putchar(i);
+	for (x= 'a'; x <= 'f'; x++)	
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
