2cf90cc385844675618d5c89eb0de84ab6fd3db1
PatrikZh <pzheji@actirana.edu.al>
2022-10-05 07:14:31 -0700

    Weird is lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * _islower - checks for lowercase character * @c: the character to check
+ * _islower - checks for lowercase character
+ * @c: the character to check
  *  Return: 1 if c is lowercase, 0 otherwise
  */
 
