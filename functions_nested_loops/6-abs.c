6fb53822626a9738bf3612bf0a15923119059366
PatrikZh <pzheji@actirana.edu.al>
2022-10-06 02:39:02 -0700

    Absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - checks for alphabetic character
+ * @n: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _abs(int n)
+{	
+	if (n >= 0)
+		return (n);
+	else 
+		return (n * (-1));
+}
