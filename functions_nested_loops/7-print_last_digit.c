710780a1ffb97044af2e4d3f0a7a52560d42489c
PatrikZh <pzheji@actirana.edu.al>
2022-10-06 04:31:03 -0700

    Absolute value with last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - computes the absolute value
+ * @n: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int print_last_digit(int n)
+
+{	int digit;
+	if (n < 0)
+
+		n = n * (-1);
+	digit = n % 10;
+	return (digit);
+}
