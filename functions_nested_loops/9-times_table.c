9d39cc27a1a4edf0b40705024fa873863ade63f9
PatrikZh <pzheji@actirana.edu.al>
2022-10-07 03:45:47 -0700

    Forming a times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,20 +17,20 @@ void times_table(void)
 			{
 				_putchar('0');
 			}
-		}
-		if (result < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(result + '0');
-		}
-		else if (result >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((result / 10) + '0');
-			_putchar((result % 10) + '0');
+			if (result < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
