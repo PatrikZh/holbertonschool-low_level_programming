503aab7e14401737a593f46b4a16cbccf3d9d0cd
PatrikZh <pzheji@actirana.edu.al>
2022-10-07 07:38:53 -0700

    List to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_to_98 - print to the given number
+ * @n: the given integer
+ */
 void print_to_98(int n)
 {
 	int i;
+
 	if (n < 98)
 	{
 		for (i = n; i < 98; i++)
@@ -18,4 +22,4 @@ void print_to_98(int n)
 			printf("%d, ", i);		}
 	}
 	printf("98\n");
-}	
+}
