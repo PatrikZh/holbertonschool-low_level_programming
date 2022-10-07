11f7982d3e36c251211aab420856cc9dc5ae530c
PatrikZh <pzheji@actirana.edu.al>
2022-10-07 03:34:39 -0700

    Forming a times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - print time
+ */
+void times_table(void)
+{
+	int i, j;
+	int result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+		}
+		if (result < 10 && j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(result + '0');
+		}
+		else if (result >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
