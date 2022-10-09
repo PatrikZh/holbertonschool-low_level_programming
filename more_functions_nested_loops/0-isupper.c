b6f49b446e119992e480eb9cec75aabd7d27039d
PatrikZh <pzheji@actirana.edu.al>
2022-10-09 16:50:02 -0700

    To check for uppercase letters

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * isupper - the funciton that chekcs for uppercase leters
+ * @c: the parameter
+ * Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	return ((c >= 'A' && c <= 'Z'));
+
+
+}
