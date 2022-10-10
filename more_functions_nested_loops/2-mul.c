c7e12e76a402ed0ca167172a9bebc119fb073fda
PatrikZh <pzheji@actirana.edu.al>
2022-10-09 17:32:24 -0700

    Multiplicate two numebrs/integers together

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul  - the function that multiplicates
+ * @a: the given components
+ * @b: the given components
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
