c8a031349c1dae637e42c044543c5ca1dc62958f
PatrikZh <pzheji@actirana.edu.al>
2022-10-09 19:17:15 -0700

    Square table

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,14 @@
 
 void print_square(int size)
 {
-
 	int x, y;
 
-	for (x = 0; x < size; x++)
+	for (x = 0; x< size; x++)
 	{
-	for (y = 0; y < size; y++)
-	{		_putchar('#');
+		for (y = 0; y < size; y++)
+			_putchar('#');
 		_putchar('\n');
 	}
-
-	}
-
 	if (size <= 0)
 		_putchar('\n');
-
 }
-
