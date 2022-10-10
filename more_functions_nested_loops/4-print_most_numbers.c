4e4674631421320fe852a54bb8890f4e5cf9363b
PatrikZh <pzheji@actirana.edu.al>
2022-10-09 18:06:34 -0700

    Minus 2 and 4 from the sequence

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers  - the function that multiplicates
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		{
+			if (a != '2' && '4')
+				_putchar(a);
+		}
+				_putchar('\n');
+}
