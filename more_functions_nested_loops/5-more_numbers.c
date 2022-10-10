39f5fcdccb9046284ef25a5ba99ece0daa212ed0
PatrikZh <pzheji@actirana.edu.al>
2022-10-09 18:28:47 -0700

    Print it 14 time's

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more numbers - more numbers indeed
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+				_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
