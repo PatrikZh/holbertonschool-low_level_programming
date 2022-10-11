33596faa9941fb717eb1470670b3f39b5a167be1
PatrikZh <pzheji@actirana.edu.al>
2022-10-11 03:11:54 -0700

    Swap numbers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,16 @@
 #include "main.h"
+
 /**
- *
+ * swap_int - to swap the given integers
+ * @a: the given value
+ * @b: the given value
  */
+
 void swap_int(int *a, int *b)
 {
-	
+	int Sharin;
 
+	Sharin = *a;
+	*a = *b;
+	*b = Sharin;
 }
