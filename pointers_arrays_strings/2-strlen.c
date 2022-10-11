f01cddb138c3d74be02694b2f4b6c64388ec928c
PatrikZh <pzheji@actirana.edu.al>
2022-10-11 03:40:31 -0700

    Print the legnth of a string

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 
 /**
- *
+ * _strlen - strlen library
+ * prints the length
+ * of a string by itself.
+ * @s: the given value
+ * Return: 1++ gives goes
+ * on and on for the length
+ * of the string
  */
 
 int _strlen(char *s)
 {
- 	int i;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
