928dcee9492624f73c65eb922cab7f7c95cd62c5
PatrikZh <pzheji@actirana.edu.al>
2022-10-12 06:40:52 -0700

    Only keep those with 0 reminder (%2)

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != '1' && str[i] != '3' && str[i] != '5' && str[i] != '7' && str[i] != '9')
+		if (i % 2 == 0 )
 			_putchar(str[i]);
 	}
 		_putchar('\n');
