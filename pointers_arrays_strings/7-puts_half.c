7cbdb287e1695b5a18ec1c98f9e54ee30a85d318
PatrikZh <pzheji@actirana.edu.al>
2022-10-12 06:45:08 -0700

    Only half of it

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - the function
+ * @str: the given value
+ */
+
+void puts2(char *str)
+{
+        int i;
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                if (i / 2 == 0)
+                        _putchar(str[i]);
+        }
+                _putchar('\n');
+}
+
