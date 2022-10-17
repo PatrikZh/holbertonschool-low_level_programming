2962ec2888136749355106067711d22490bf6153
PatrikZh <pzheji@actirana.edu.al>
2022-10-16 17:37:46 -0700

    Morse Code

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,5 +1,26 @@
 #include "main.h"
-  
+
 /**
- *
+ *leet - the function
+ *@a: the given value
+ *Return: return result
  */
+
+char *leet(char *a)
+{
+	int i, u;
+	char *x = "aAeEoOtTlL";
+	char *y = "4433007711";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (u = 0; u < 10; u++)
+		{
+			if (a[i] == x[u])
+			{
+				a[i] = y[u];
+			}
+		}
+	}
+	return (a);
+}
