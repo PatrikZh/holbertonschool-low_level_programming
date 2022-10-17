2f5dfb7be61b9de62b4d49372ecd7661817f8657
PatrikZh <pzheji@actirana.edu.al>
2022-10-16 17:01:36 -0700

    Capitalizes all words of a string

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+	if (a[i] >= 'a' && a[i] <= 'z')
+	{
+		a[i] -= 32;
+	}
+	}
+	return (a);
+}
