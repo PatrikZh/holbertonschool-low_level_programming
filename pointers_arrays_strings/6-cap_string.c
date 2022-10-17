93ddc97e1c873a2d3b9eb41946f2b69f94921529
PatrikZh <pzheji@actirana.edu.al>
2022-10-16 17:37:21 -0700

    Wtf was this geez

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,41 @@
 #include "main.h"
-  
+
 /**
- *
+ * cap_string - the function
+ * @a: the given value
+ * Return: return result
  */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	if (a[i] >= 'a' && a[i] <= 'z')
+
+		a[i] -= 32;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+
+		if (a[i] > 0 && a[i] < 47 && a[i] != '-')
+		{
+			i++;
+
+			if (a[i] >= 'a' && a[i] <= 'z')
+
+				a[i] -= 32;
+			i--;
+
+		}
+		if (a[i] > 122 || (a[i] > 57 && a[i] < 65))
+		{
+			i++;
+
+			if (a[i] >= 'a' && a[i] <= 'z')
+
+				a[i] -= 32;
+			i--;
+		}
+	}
+	return (a);
+}
