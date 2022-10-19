bd584d46b0ce5b83acdc4c0b024dea28029bec9b
PatrikZh <pzheji@actirana.edu.al>
2022-10-19 05:29:08 -0700

    Return factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 /**
- *
+ * factorial- function
+ * @n: the given value
+ * Return: result
  */
 
 int factorial(int n)
 {
 	if (n < 0)
-	{	
+	{
 		return (-1);
 	}
 	if (n == 0)
@@ -16,7 +18,7 @@ int factorial(int n)
 		return (1);
 	}
 	else
-        {
-                return (n * factorial(n-1));
-        }
+	{
+	return (n * factorial(n - 1));
+	}
 }
