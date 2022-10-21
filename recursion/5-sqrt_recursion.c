4d988eaef0ae68c5b17aea198e915c9fdffe594e
PatrikZh <patrik.zheji@gmail.com>
2022-10-21 06:50:31 -0700

    return root natural number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,14 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+int _sqrt(int n, int i);
 
 /**
- * _sqrt_recursion- function
+ * _sqrt- function
  * @n: the given value
- * Return- result
+ * @i: the given value
+ * Return: result
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt(n, i + 1));
+}
+
+/**
+ *_sqrt_recursion - function
+ * @n: the given value
+ * Return: result
  */
 
 int _sqrt_recursion(int n)
 {
-	
-
+	return (_sqrt(n, 0));
 }
