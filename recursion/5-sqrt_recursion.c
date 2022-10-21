26d803c20a1a3c781fa9c7cc7ba2ec4ed1dfa3c9
PatrikZh <patrik.zheji@gmail.com>
2022-10-21 06:10:42 -0700

    Recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion- function
+ * @n: the given value
+ * Return- result
+ */
+
+int _sqrt_recursion(int n)
+{
+	
+
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number- function
+ * @n: the given value
+ * Return- result
+ */
+
+int is_prime_number(int n)
+{
+	
+
+}
