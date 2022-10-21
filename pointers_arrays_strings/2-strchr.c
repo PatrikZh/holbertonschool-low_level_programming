ca6bdcf314a0024c598a77092dbb65b84663bdfe
PatrikZh <patrik.zheji@gmail.com>
2022-10-21 09:15:20 -0700

    aint right

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - function
+ * @s: the given value
+ * @c: the given value
+ * Return: result
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
