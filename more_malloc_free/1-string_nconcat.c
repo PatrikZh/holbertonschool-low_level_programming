0829ee4f8053358cf3e892be9ec6f13d93b103f5
PatrikZh <patrik.zheji@gmail.com>
2022-10-26 06:26:56 -0700

    Combine

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
  * string_nconcat - function
  * @s1: the given value
@@ -14,27 +14,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1, len2, i;
 	char *pointer;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
 	}
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{
 	}
-	if (n <= len2)
+	if (n < len2)
 		len2 = n;
-
-	pointer = malloc((len1 + len2) * sizeof(char));
+	pointer = malloc((len1 + len2 + 1) * sizeof(char));
 	if (pointer == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
+	for (i = 0; *s1 != '\0'; i++)
 	{
-		pointer[i] = s1[i];
+		pointer[i] = *s1;
+		s1++;
 	}
-	for (i = len1; i < len1 + len2; i++)
+	for (i = len1; *s2 != '\0' && n != 0; i++)
 	{
-		pointer[i] = s2[i - len2];
+		pointer[i] = *s2;
+		s2++;
+		n--;
 	}
-
+	pointer[i] = '\0';
 	return (pointer);
 }
