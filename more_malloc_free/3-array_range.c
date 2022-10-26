0f0e1373538fd8a05cc7b04b022939efbdcec389
PatrikZh <patrik.zheji@gmail.com>
2022-10-26 16:50:31 -0700

    Create an array of integers

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,19 +13,19 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int i;
-	int size = max - min + 1;
+	int size = (max - min + 1);
 
 	if (min > max)
 		return (NULL);
 	arr = malloc(size * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
-
 	i = 0;
 	while (i < size)
 	{
 		arr[i] = min;
 		min++;
+		i++;
 	}
 	return (arr);
 }
