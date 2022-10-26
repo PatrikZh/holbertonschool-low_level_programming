7545689e17c0cdd1aa4c64d35d34e4db13734e4a
PatrikZh <patrik.zheji@gmail.com>
2022-10-26 05:54:15 -0700

    Combine

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{
 	}
-	if (n < len2)
+	if (n <= len2)
 		len2 = n;
 
 	pointer = malloc((len1 + len2) * sizeof(char));
