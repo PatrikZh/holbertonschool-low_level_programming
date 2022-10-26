3648db8f2ba3fade5377d64ff07e602b3f4aaba8
PatrikZh <patrik.zheji@gmail.com>
2022-10-26 16:06:57 -0700

    Create an array of integers

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
  * array_range - function
@@ -9,5 +11,20 @@
 
 int *array_range(int min, int max)
 {
-	
+	int *arr;
+	int i;
+	int size = max - min + 1;
+
+	if (min > max)
+		return (NULL);
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = min;
+		min++;
+	}
+	return (arr);
 }
