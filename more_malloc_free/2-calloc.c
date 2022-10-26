3fe5c993f13760bbcc6caeb16b8ee86c61d39943
PatrikZh <patrik.zheji@gmail.com>
2022-10-26 14:58:37 -0700

    Using calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-  
+#include <stdlib.h>
+#include "main.h"
+
 /**
  * _calloc - function
  * @nmemb: the given value
@@ -9,5 +11,12 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-		
+	char *i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	i = calloc(nmemb, size);
+	if (i == NULL)
+		return (NULL);
+	return (i);
 }
