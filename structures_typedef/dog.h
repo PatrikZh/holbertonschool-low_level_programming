014638444be6691c03eefdcba4d75f99b5f5776c
PatrikZh <patrik.zheji@gmail.com>
2022-10-31 06:26:25 -0700

    WOOF

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - funtion
+ * @name: the given value
+ * @age: the given value
+ * @owner: the given value
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 #endif
