6563ff776bcd93d7512e544523317c8b568d933f
PatrikZh <patrik.zheji@gmail.com>
2022-11-03 08:59:24 -0700

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 
 /**
- *
+ * main - function
+ * @argc: parameter
+ * @argv: double pointer/parameter
+ * Return: Always 0.
  */
 
 int main(__attribute__((unused))int argc, char **argv)
 {
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
