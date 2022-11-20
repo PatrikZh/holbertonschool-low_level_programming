d5a53d7d63aaaf6bcbe9980919eb2c85cb06c3ea
PatrikZh <patrik.zheji@gmail.com>
2022-11-20 12:46:59 -0800

    Write a function that deletes the head node of a listint_t linked list, and returns the head node’s data

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -11,13 +11,13 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n = 0;
 
-	while (*head)
+
+	if (*head)
 	{
 		n = (*head)->n;
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
-		n++;
 	}
 	return (n);
 }
