6485f90674dbc82f149c7a54ad6fff0241d28c8f
PatrikZh <patrik.zheji@gmail.com>
2022-11-21 07:50:22 -0800

    Write a function that prints anything; char,int,float,char *

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,14 +1,57 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - Prints a char
+ * @ap: Argument pointer
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int - Prints an integer
+ * @ap: Argument pointer
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - Prints a float
+ * @ap: Argument pointer
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_str - Prints a string
+ * @ap: Argument pointer
+ */
+void print_str(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (!s)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
+
+}
+/**
  * print_all - function
  */
 
 void print_all(const char * const format, ...)
 {
-	int i; 
+	unsigned int i;
 	char *s;
-	
+
 	va_list(arg);
 
 	va_start(arg,format);
@@ -17,17 +60,24 @@ void print_all(const char * const format, ...)
 	i = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] == 'c')
-			printf("%c", va_arg(arg, int));
-		if (format[i] == 'i')
-			printf("%d", va_arg(arg, int));
 		switch (format[i])
 		{
-			case 's'
+			case ('c'):
+				printf("%c", va_arg(arg, int));
+				break;
+			case ('i'):
+				printf("%d", va_arg(arg, int));
+				break;
+			case ('s'):
 				printf("%s", s);
-			case 'f'
+				break;
+			case ('f'):
 				printf("%f", va_arg(arg, double));
+				break;
+
 		}
-			i++;
+		i++;
 	}
+	printf("\n");
+	va_end(arg);
 }
