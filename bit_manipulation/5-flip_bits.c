ac889c086790a41f77ccee9a99c89a6ab29f9057
PatrikZh <patrik.zheji@gmail.com>
2022-11-22 02:47:06 -0800

    Write a function that sets the value of a bit to 0 at a given index

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * flip_bits - function
+ * @n: parameter
+ * @m: parameter
+ * Return: result
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int value = sizeof()	
+}
