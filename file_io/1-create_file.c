08780b28dbf71857961c0bbd2714b992e81bc586
PatrikZh <patrik.zheji@gmail.com>
2022-11-22 08:02:56 -0800

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,9 +29,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (write(fd, text_content, strlen_(text_content)) == -1)
 		return (-1);
+	if (!text_context)
+		return (1);
 	close(fd);
 	return (1);
 }
