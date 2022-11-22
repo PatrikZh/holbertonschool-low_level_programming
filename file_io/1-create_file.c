f6ced5d58cf5121c690c3356ac145c6b1072fd8f
PatrikZh <patrik.zheji@gmail.com>
2022-11-22 08:12:32 -0800

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (!text_content)
-		return(1);
+		return (1);
 	if (write(fd, text_content, strlen_(text_content)) == -1)
 		return (-1);
 	close(fd);
