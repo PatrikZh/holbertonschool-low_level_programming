75518017afb01ff5ff6292e73820e08023948135
PatrikZh <patrik.zheji@gmail.com>
2022-11-22 08:00:43 -0800

    Create a function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * strlen_ - function
+ * @s: parameter
+ * Return: result
+ */
+
+int strlen_(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * create_file - function
+ * @filename: parameter
+ * @text_content: parameter
+ * Return: result
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT);
+	if (write(fd, text_content, strlen_(text_content)) == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
