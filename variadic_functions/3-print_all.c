41d220ae4de8aa642fd1147f74504c77db2d717f
PatrikZh <patrik.zheji@gmail.com>
2022-11-22 06:00:33 -0800

    Fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -50,13 +50,10 @@ void print_str(va_list ap)
 void print_all(const char * const format, ...)
 {
 	unsigned int i;
-	char *s;
 
 	va_list(arg);
 
 	va_start(arg,format);
-
-	s = va_arg(arg, char *);
 
 	i = 0;
 	while (format[i] != '\0')
