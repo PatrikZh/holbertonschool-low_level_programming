3c4d99bcdc9662fd66d3f4894ba77f3fc4703555
PatrikZh <patrik.zheji@gmail.com>
2022-11-24 04:43:29 -0800

    Insert a new node at the beginning

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function
+ * @head: double pointer parameter
+ * @n: parameter
+ * Return: result
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
