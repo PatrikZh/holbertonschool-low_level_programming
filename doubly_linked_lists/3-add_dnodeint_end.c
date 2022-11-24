a3ac586fa20acf7887171b9db817a4065bfcc179
PatrikZh <patrik.zheji@gmail.com>
2022-11-24 06:55:04 -0800

    Insert a new node at the end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,7 +19,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = NULL;
 	last_node = *head;
 	if (*head == NULL)
 		*head = new_node;
@@ -30,7 +29,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			last_node = last_node->next;
 		}
 		last_node->next = new_node;
-	}
+	}	
+	new_node->prev = last_node;
 	return (new_node);
 
 }
