40d844c1fbb97bd002338f97532046d09af26c90
PatrikZh <patrik.zheji@gmail.com>
2022-11-25 10:54:47 -0800

    Fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temporary, *pointer;
-	unsigned int count;
+	unsigned int count = 0;
 
 	pointer = *h;
 	if (idx == 0)
