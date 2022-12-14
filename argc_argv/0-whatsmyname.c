bdd5661c8b8148da7a8dccbf527b037b1b77fb0e
PatrikZh <patrik.zheji@gmail.com>
2022-12-14 04:37:15 -0800

    ARGC/ARGV print its own name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- *
+ * main - function of printing its own name
+ * @argc: stores & passses arguments on the command line
+ * @argv: arrays containg strings
+ * Return: 0 Success.
  */
 
 int main(int __attribute__ ((unused)) arg, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
