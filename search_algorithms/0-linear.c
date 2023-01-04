d509c648c5b34e214e29325dd535e2895ac43fb1
PatrikZh <patrik.zheji@gmail.com>
2023-01-04 02:21:42 -0800

    Adding

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search linear for an specific value
+ * @array: the array that contains the values
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	
+}
